3821cc80e46682c603a1f389dc0fe8982fe501dc
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-24 10:35:24 +0300

    Learning

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,12 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :content, :short_content
+  attributes :title, :content
 
   belongs_to :author
   has_many :post_tags
   has_many :tags, through: :post_tags
 
-  def short_content
-    "#{self.object.content[0..39]}..."
-  end
-  
+ 
+
 end
